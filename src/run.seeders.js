9b2fbaba10a1ab95b863eb7fe98792ba9b766df0
JoseLu2121 <josmorver@alum.us.es>
2026-04-13 16:16:39 +0200

    feat: elden ring seeder added

--- src/run.seeders.js
+++ src/run.seeders.js
@@ -1,11 +1,13 @@
 import { SeederManager } from "./seeder.manager.js";
 import { ItemsSeeder } from "./seeders/seeder.example.js";
 import { LeagueItemsSeeder } from "./seeders/seeder.lol.js";
-
+import { EldenRingItemsSeeder } from "./seeders/seeder.eldenring.js";
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017";
 
 const manager = new SeederManager(MONGO_URI);
 
 manager.addSeeder(new ItemsSeeder());
 manager.addSeeder(new LeagueItemsSeeder());
+manager.addSeeder(new EldenRingItemsSeeder());
+
 manager.executeAll();
