094338a943c8fd129b701d9f227ad2c1b99ad7fc
JoseLu2121 <josmorver@alum.us.es>
2026-04-13 16:14:06 +0200

    Revert "feat: elden ring api done"
    This reverts commit 0d3d4e4810512784768f5b6be82859bdfab9b0b3.

--- src/run.seeders.js
+++ src/run.seeders.js
@@ -1,7 +1,6 @@
 import { SeederManager } from "./seeder.manager.js";
 import { ItemsSeeder } from "./seeders/seeder.example.js";
 import { LeagueItemsSeeder } from "./seeders/seeder.lol.js";
-import { EldenRingItemsSeeder } from "./seeders/seeder.eldenring.js";
 
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017";
 
@@ -9,5 +8,4 @@ const manager = new SeederManager(MONGO_URI);
 
 manager.addSeeder(new ItemsSeeder());
 manager.addSeeder(new LeagueItemsSeeder());
-manager.addSeeder(new EldenRingItemsSeeder());
 manager.executeAll();
